7545223c5a1bfc894299741a853203ed2a32f0ab
dkurgan <dunkanup@gmail.com>
2020-10-29 20:24:23 -0700

    upd text

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
     return (
       <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "90vh", flexDirection: 'column'}}>
         <h2 style={{ fontWeight: "bold", color: "gray" }}>Hello, thank you for interesting</h2>
-        <h3 style={{ fontWeight: "bold", color: "gray"}}>Currently we are not life yet</h3>
+        <h3 style={{ fontWeight: "bold", color: "gray"}}>Currently we are under construction</h3>
       </div>
     )
   }
