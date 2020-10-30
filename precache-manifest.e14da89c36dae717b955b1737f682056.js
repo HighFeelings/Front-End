ddfc1d9b0148d2df234e62933b466e569e16846e
dkurgan <dunkanup@gmail.com>
2020-10-29 20:06:35 -0700

    Updates

---
+++ precache-manifest.e14da89c36dae717b955b1737f682056.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3172815ecfb86ab2073dc73e46f2ae7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b59e777f76bc9c92e076",
+    "url": "/static/css/main.47ce7e00.chunk.css"
+  },
+  {
+    "revision": "d634b92862562113eb0e",
+    "url": "/static/js/2.fe640a42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fe640a42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b59e777f76bc9c92e076",
+    "url": "/static/js/main.df13d076.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
