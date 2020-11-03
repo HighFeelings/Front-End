d01d08b7516aa45eddfc1856654adbef570b6f7f
dkurgan <dunkanup@gmail.com>
2020-11-02 18:06:23 -0800

    upd fonts

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ export default class App extends React.Component {
   render() {
     return (
       <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "90vh", flexDirection: 'column'}}>
-        <h2 style={{ fontWeight: "bold", color: "gray" }}>Hello, thank you for interesting</h2>
-        <h3 style={{ fontWeight: "bold", color: "gray"}}>Currently we are under construction</h3>
+        <h2 style={{color: "lightgray" }}>Hello, thank you for interesting</h2>
+        <h3 style={{color: "lightgray" }}>Currently we are under construction</h3>
       </div>
     )
   }
