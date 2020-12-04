e9d9ec711b1deaaa07cded66f7b8009422beabfc
dkurgan <dunkanup@gmail.com>
2020-12-04 12:17:51 -0800

    upd colors

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ export default class App extends React.Component {
   render() {
     return (
       <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "90vh", flexDirection: 'column'}}>
-        <h2 style={{color: "lightgray" }}>Hello, thank you for interesting</h2>
-        <h3 style={{color: "lightgray" }}>Currently we are under construction</h3>
+        <h2 className="construction-fonts">Hello, thank you for interest</h2>
+        <h3 className="construction-fonts">Currently we are under construction</h3>
       </div>
     )
   }
